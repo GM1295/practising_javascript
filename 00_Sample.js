ec621bc8a09bd813c53175f841cba30ff413c19d
SAURABH <saurabh.d.0102@gmail.com>
2023-08-24 17:22:52 +0530

    added function produceTable

--- 00_Sample.js
+++ 00_Sample.js
@@ -3,3 +3,9 @@ console.log(a)
 const greet=function(name,msg){
 Console.log(`hi ${name} ${msg}`)
 }
+const produceTabel=(num)=>{for(let i=1;i<=10;i++)
+  {
+    console.log(`${num} * ${i}  = ${num*i}`)
+  }
+  }
+produceTable(23);
